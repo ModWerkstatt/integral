d36df838e35c6bbc9f93239e39be0a70a49f6d80
Florian Thiel <flth@flth.de>
2024-07-10 01:08:15 +0200

    initial

---
+++ mod.lua
@@ -0,0 +1,26 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 1,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "train", "locomotive", "engine", "diesel" },
+		dependencies = { },
+		url = { "" },
+	},
+	options = {
+	},
+
+	runFn = function (settings)
+
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,34 @@
+﻿function data()
+	return {
+		de = {
+			["mod_name"] = "Integral BR609.1",
+			["mod_desc"] = "Die V 160 war die Prototypslok der späteren Baureihe 21x. 1956 begannen die Entwicklungsarbeiten und 1960 konnte mit den Testfahrten begonnen werden. \n" ..
+				"Insgesamt wurden 10 Stück gebaut. Den Spitznamen Lollo erhielt die Lok aufgrund der rundlichen Frontpartie ähnlich der damals berühmte Gina Lollobrigida.\n" ..
+				"- Höchstgeschwindigkeit: 120 km/h \n" ..
+				"- Leistung: 1400 KW \n" ..
+				"- Verfügbarkeit: 1960 bis 2003",
+            ["v160_menu"] = "V 160 / BR 216 \"Lollo\"",
+            ["v160_name"] = "V 160",
+            ["v160_desc"] = "Es wurden 10 Prototypen geliefert, die mit dem ursprünglichen, rundlichen Design an der Front gestaltet waren. Für die Serienlieferung wurde dann auf ein kantigeres Design zurückgegriffen, wie es alle BR 216 und BR 218 zeigen. Die rundliche Form brachte den Maschinen den Spitzname \"Lollo\" (in Anspielung auf Gina Lollobrigida) ein.",
+            ["lollo_br216_name"] = "BR 216",
+            ["lollo_br216_desc"] = "Ab 1968 wurden die Prototypen ebenso wie die anderen Mitglieder der V 160-Familie mit einer Computernummer versehen. Die Lollo bekam die Baureihenbezeichnung 216 und verblieb bis ca. 1984 im aktiven Dienst. Die Serienloks liefen noch 20 Jahre länger.",
+            ["v160_museum_name"] = "V 160 Museumslok",
+            ["v160_museum_desc"] = "Die V 160 002 steht im altroten Lack auch heute noch bei Traditionsfahrten im Einsatz.",
+		},
+		en = {
+			["mod_name"] = 	"Integral BR609.1",
+			["mod_desc"] = 	"The  V 160 was the prototype of the later class 21x. In 1956 started the development and 1960 was the beginning of first testruns.\n" ..
+				"Totally were 10 V 160 prototypes built. The nick name 'Lollo' became the locomotive because of the roundish front similar to the famous Gina Lollobrigida.\n" ..
+				"- Vmax			: 75 mph\n" ..
+				"- Power		: 1400 KW\n" ..
+				"- Availability 	: 1960 to 2003",
+            ["v160_menu"] = "V 160 / BR 216 \"Lollo\"",
+            ["v160_name"] = "V 160",
+            ["v160_desc"] = "10 prototypes were delivered, which were designed with the original, rounded design on the front. A more angular design was then used for series delivery, as shown by all class 216 and class 218. The rounded shape gave the machines the nickname \"Lollo\" (alluding to Gina Lollobrigida).",
+            ["lollo_br216_name"] = "Class 216",
+            ["lollo_br216_desc"] = "From 1968, the prototypes, like the other members of the V 160 family, were given a computer number. The Lollo got the series designation 216 and remained active until about 1984. The production locomotives ran 20 years longer.",
+            ["v160_museum_name"] = "V 160 Museum Engine",
+            ["v160_museum_desc"] = "The V 160 002 is in service for museum trains in the original purple live until today.",
+		},
+	}
+end
