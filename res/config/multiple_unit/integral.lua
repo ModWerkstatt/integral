ec239f660edf12fcfce7c2ecf8d71d84dc51c439
Florian Thiel <flth@flth.de>
2024-07-11 00:52:41 +0200

    Kopf nochmal umgebaut

---
+++ res/config/multiple_unit/integral.lua
@@ -0,0 +1,15 @@
+function data()
+return {
+	vehicles = {
+		{ name = "vehicle/train/br609.1/endwagen1_bob.mdl", forward = false },
+--		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_mw.mdl", forward = true },
+--		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_mw.mdl", forward = true },
+--		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_mw.mdl", forward = true },
+--		{ name = "vehicle/waggon/skodapushpull/SkodaDosto_mw.mdl", forward = true },
+		{ name = "vehicle/train/br609.1/endwagen2_bob.mdl", forward = true },
+	},
+	name = _("integral_name"),
+	desc = _("integral_desc"),
+    groupFileName = "vehicle/train/br609.10/menu_integral.mdl",
+}
+end
