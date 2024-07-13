49a100e8056195c8ade4d28e75e95c6ed493da3e
Florian Thiel <flth@flth.de>
2024-07-14 01:26:41 +0200

    BOB Zug fast fertig, innen noch nicht lackiert

---
+++ res/config/multiple_unit/integral_bob.lua
@@ -0,0 +1,14 @@
+function data()
+return {
+	vehicles = {
+		{ name = "vehicle/train/br609.1/endwagen1_bob.mdl", forward = false },
+		{ name = "vehicle/train/br609.1/fahrgastzelle21_bob.mdl", forward = true },
+   		{ name = "vehicle/train/br609.1/laufwagen_bob.mdl", forward = true },
+        { name = "vehicle/train/br609.1/fahrgastzelle22_bob.mdl", forward = false },
+		{ name = "vehicle/train/br609.1/endwagen2_bob.mdl", forward = true },
+	},
+	name = _("integral_name"),
+	desc = _("integral_desc"),
+    groupFileName = "vehicle/train/br609.10/menu_integral.mdl",
+}
+end
