9c45286139cd65064d4ce9dbc2c09bce0116ab53
Florian Thiel <flth@flth.de>
2024-07-19 20:13:31 +0200

    Türsound überarbeitet

--- res/config/sound_set/integral_fahrgastzelle.lua
+++ res/config/sound_set/integral_fahrgastzelle.lua
@@ -1,15 +1,24 @@
 local soundsetutil = require "soundsetutil"
 
 function data()
+return {
+    tracks = {
+	},
+    events = {
+		openDoors = { names = { "vehicle/train/br609.1/tur_auf_integral.wav" }, refDist = 10.0 },
+		closeDoors = { names = { "vehicle/train/br609.1/tur_zu_integral.wav" }, refDist = 10.0 },
+	},
 
-local data = soundsetutil.makeSoundSet()
+    updateFn = function (input)
+		return {
+			tracks = {
 
-
-
-soundsetutil.addEvent(data, "openDoors", { "vehicle/train/br609.1/tur_auf_integral.wav" }, 5.0)
-soundsetutil.addEvent(data, "closeDoors", { "vehicle/train/br609.1/tur_zu_integral.wav" }, 5.0)
-
-
-return data
-
+			},
+			events = {
+				openDoors = { gain = 1.00, pitch = 1.0 },
+				closeDoors = { gain = 1.00, pitch = 1.0 },
+			}
+		}
+	end
+}
 end
